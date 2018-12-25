2200f0b8b82564f87da7d55edf52d9bff5fc930d
Takanori Watanabe <takanori5064@gmail.com>
2018-12-25 15:18:19 +0900

    コメントの削除

--- docs/assets/speech2vrc.js
+++ docs/assets/speech2vrc.js
@@ -14,17 +14,9 @@ function recognize() {
     recognition.addEventListener("result", e => {
         input = e.results[0][0].transcript;
         recognition.stop();
-        /*
-        if(input !== defaultInput) {
-            console.log(input);
-            document.getElementById("recognizedText").textContent = input;
-            input = defaultInput;
-        }
-        */
     });
 
     // 聞き取り終了時のイベントリスナー
-    
     recognition.addEventListener("end", () => {
         if(input === defaultInput) {
             recognition.start()
@@ -36,7 +28,6 @@ function recognize() {
         }
     });
     
-
     // 聞き取りを開始する
     recognition.start();
 }
