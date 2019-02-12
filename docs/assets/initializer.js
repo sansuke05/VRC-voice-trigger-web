0bb0a6fdb9d4c36dd552cfe8ee45caebc20f17be
Takanori Watanabe <takanori5064@gmail.com>
2019-02-13 00:52:14 +0900

    [add]ワールドリストの情報をhtmlに設定する機能を追加

--- docs/assets/initializer.js
+++ docs/assets/initializer.js
@@ -1,4 +1,14 @@
 // ワールドリストの情報をhtmlに設定し、選択できるようにする
 var setWorldInfo = function(worldList) {
-    
+    index = 0;
+
+    worldList.forEach(world => {
+        let option = document.createElement("option");
+        option.value = '' + index;
+        option.text = world;
+        document.getElementById("worldNames").appendChild(option);
+
+        console.log(index);
+        index++;
+    });
 }
